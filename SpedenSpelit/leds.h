3658e0ef966fb46f9594614afc7e7a1409ff137a
Jookarja <Joonash.karjalainen@gmail.com>
2024-11-26 15:21:48 +0200

    Update leds.h

--- SpedenSpelit/leds.h
+++ SpedenSpelit/leds.h
@@ -1,51 +1,17 @@
-#ifndef LEDS_H
-#define LEDS_H
-#include <arduino.h>
+#ifndef LEDCONTROL_H
+#define LEDCONTROL_H
 
-/*
-  initializeLeds() subroutine intializes analog pins A2,A3,A4,A5
-  to be used as outputs. Speden Spelit leds are connected to those
-  pins.  
-*/
-void initializeLeds();
+#include <Arduino.h>
 
-/*
-  setLed(byte) sets correct led number given as 0,1,2 or 3
-  led number 0 corresponds to led connected at Arduino pin A2
-  led number 1 => Arduino pin A3
-  led number 2 => Arduino pin A4
-  led number 3 => Arduino pin A5
-  
-  parameters:
-  byte ledNumber is 0,1,2 or 3
-*/
-void setLed(byte ledNumber);
+const byte ledPins[] = {A2, A3, A4, A5};
+const int ledCount = sizeof(ledPins) / sizeof(ledPins[0]);
 
-/*
-  clearAllLeds(void) subroutine clears all leds
-*/
-void clearAllLeds(void);
-
-/*
-  setAllLeds subroutine sets all leds
-*/
-void setAllLeds(void);
-
-/*
-  show1() subroutine shows numbers 0,1,...,15 as binary numbers
-  waits a bit and repeats number "show"
-*/
-void show1(void);
-
-/*
-  show2(int) subroutine shows leds 0,1,2,3,0,1,2,3,.... with increasing
-  changing rate. And when top change speed has been reached
-
-    Parameters:
-  
-  int rounds: This parameter determines how many times 0,1,2,3 sequence
-              is shown. 
-*/
-void show2(int);
+// Funktiot
+void setupLeds();
+void setLed(byte index);
+void clearAllLeds();
+void setAllLeds();
+void show1();
+void show2();
 
 #endif
